13d82e2e60eb5d9f93815dc3041091b869a01638
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2020-01-24 22:07:30 +0300

    Change to raise_for_status

--- main.py
+++ main.py
@@ -21,24 +21,21 @@ def createParser():
 def download_txt(url, filename, folder='books/'):
     os.makedirs(folder, exist_ok=True)
     response = requests.get(url, allow_redirects=False)
-    if response.status_code == 200:
-        response.raise_for_status()
-        filepath = os.path.join(folder, sanitize_filename(filename) + '.txt')
-        with open(filepath, 'w', encoding='utf8') as file:
-            file.write(response.text)
-        return filepath
+    response.raise_for_status()
+    filepath = os.path.join(folder, sanitize_filename(filename) + '.txt')
+    with open(filepath, 'w', encoding='utf8') as file:
+        file.write(response.text)
+    return filepath
 
 
 def download_image(url, filename, folder='images/'):
     os.makedirs(folder, exist_ok=True)
     response = requests.get(url, allow_redirects=False)
-    if response.status_code == 200:
-        response.raise_for_status()
-        filepath = os.path.join(folder, sanitize_filename(filename))
-        with open(filepath, 'wb') as file:
-            file.write(response.content)
-        return filepath
-    pass
+    response.raise_for_status()
+    filepath = os.path.join(folder, sanitize_filename(filename))
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
+    return filepath
 
 
 def get_title_and_author_from_soup(soup):
@@ -52,13 +49,13 @@ def get_title_and_author_from_soup(soup):
 
 def get_comments_from_soup(soup):
     comments_tags = soup.select('div.texts span')
-    comments = [comment_tag for comment_tag in comments_tags]
+    comments = [comment_tag.text for comment_tag in comments_tags]
     return comments
 
 
 def get_genres_from_soup(soup):
     genre_tags = soup.select('span.d_book a')
-    genres = [genre_tag for genre_tag in genre_tags]
+    genres = [genre_tag.text for genre_tag in genre_tags]
     return genres
 
 
@@ -69,18 +66,17 @@ if __name__ == '__main__':
     book_urls = get_book_urls_from_category_url(FANTASTIC_CATEGORY_URL, args.start_page, args.end_page)
     for book_url in book_urls:
         response = requests.get(book_url, allow_redirects=False)
-        if response.status_code == 200:
-            response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'lxml')
-            title, author = get_title_and_author_from_soup(soup)
-            comments = get_comments_from_soup(soup)
-            genres = get_genres_from_soup(soup)
-            image_url = urljoin('http://tululu.org', soup.select_one('div.bookimage img')['src'])
-            book_id = book_url.strip('/').split('/')[-1].strip('b')
-            txt_url = f'http://tululu.org/txt.php?id={book_id}'
-            book_path = download_txt(txt_url, title)
-            image_src = download_image(image_url, image_url.split('/')[-1])
-            book = {
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        title, author = get_title_and_author_from_soup(soup)
+        comments = get_comments_from_soup(soup)
+        genres = get_genres_from_soup(soup)
+        image_url = urljoin('http://tululu.org', soup.select_one('div.bookimage img')['src'])
+        book_id = book_url.strip('/').split('/')[-1].strip('b')
+        txt_url = f'http://tululu.org/txt.php?id={book_id}'
+        book_path = download_txt(txt_url, title)
+        image_src = download_image(image_url, image_url.split('/')[-1])
+        book = {
                 'title': title,
                 'author': author,
                 'image_src': image_src,
@@ -88,7 +84,8 @@ if __name__ == '__main__':
                 'comments': comments,
                 'genres': genres,
             }
-            books.append(book)
+        books.append(book)
+
     load_dotenv()
     file_name = os.getenv('BOOK_INFORMATION_FILE_NAME')
     with open(file_name, "w", encoding='utf8') as my_file:
